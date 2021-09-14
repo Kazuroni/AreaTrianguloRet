bf11a1a48c9703501b2ab354e5e4057d7c03dbe5
Kazuroni <sayajinbr30@gmail.com>
2021-09-14 17:37:13 -0300

    FInalização

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace AreaTrianguloRet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ForegroundColor=ConsoleColor.DarkBlue;
+            Console.WriteLine("Calculadora de Area Triangulo Retangulo");
+            Console.ResetColor();
+
+            Console.WriteLine("enter para continuar");
+            Console.ReadLine();
+
+            double valorBase;
+            double valorAltura;
+
+            Console.ForegroundColor=ConsoleColor.DarkBlue;
+            Console.Write("Digite o valor da Base : ");
+            Console.ResetColor();
+            valorBase = Double.Parse(Console.ReadLine());
+
+
+            Console.ForegroundColor=ConsoleColor.DarkBlue;
+            Console.Write("Ok! Base:");
+            Console.ResetColor();
+            Console.WriteLine($" {valorBase}");
+
+            Console.WriteLine();
+
+            Console.ForegroundColor=ConsoleColor.DarkBlue;
+            Console.Write("Agora Digite a Altura...: ");
+            Console.ResetColor();
+            valorAltura = double.Parse(Console.ReadLine());
+
+            Console.ForegroundColor=ConsoleColor.DarkBlue;
+            Console.Write("a Altura é: ");
+            Console.ResetColor();
+            Console.WriteLine($" {valorAltura}");
+
+            Console.WriteLine();
+            
+            double resultado = valorBase * valorAltura;
+
+            Console.ForegroundColor=ConsoleColor.DarkBlue;
+            Console.Write($"Hmm =\\ O Resultado da Área..: ");
+            Console.ResetColor();
+            Console.WriteLine($"{resultado / 2}");            
+
+            Console.WriteLine();
+            
+            Console.ForegroundColor=ConsoleColor.DarkGreen;
+            Console.WriteLine("aperte qualque tecla para finalizar...");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
